c464b1fc40a489055e42dc975c635d5959d0c012
StefGeraets <stef.geraets@gmail.com>
2022-05-13 18:43:58 +0200

    Tick off assignment

--- src/poker/poker.ts
+++ src/poker/poker.ts
@@ -15,5 +15,4 @@ henk.draw("3");
 piet.draw("8");
 
 // [x] Maak van getCards() een promise, die resolved als alle kaarten bekend zijn
-// [ ] definieer op create poker game met typescript generics de type van de kaart (number, of “1” | “3" | “:coffee:️“) en zorg dat dit type dan dus ook op de ‘draw’ functie gebruikt wordt
-// Geef aan createPokerGame() een argument mee welke de draw functie middels generic type typed.
+// [x] definieer op create poker game met typescript generics de type van de kaart (number, of “1” | “3" | “:coffee:️“) en zorg dat dit type dan dus ook op de ‘draw’ functie gebruikt wordt
